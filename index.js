bd20770462b01bfd7c6b2ea7851b4b6f7490f254
Suresh5026 <suresh361994@gmail.com>
2024-08-03 23:15:10 +0530

    latest

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app =express();
 connectMongodb();
 app.use(bodyParser.json()); 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://resturantappreat.netlify.app', 
     credentials: true
 }));
 app.use('/user',require('./Models/userController'));
